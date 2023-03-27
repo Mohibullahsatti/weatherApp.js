062f93c80dafa6fe65915f7ff67e2b2d2b9c4d3e
Mohibullahsatti <mohibullahsatti@gmail.com>
2023-03-27 18:13:53 +0500

    Version2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import axios from "axios";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faWater, faWind, faSearch, faLocationDot, faBars, faSun } from '@fortawesome/free-solid-svg-icons';
@@ -7,70 +7,91 @@ function App() {
   const [data, setData] = useState({});
   const [location, setLocation] = useState('');
 
-  const searchLocation = (event) => {
-    if (event.key === 'Enter') {
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const { latitude, longitude } = position.coords;
+        const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=YOUR_GOOGLE_MAPS_API_KEY`;
+        axios.get(url).then((response) => {
+          const address = response.data.results[0].formatted_address;
+          setLocation(address);
+        });
+      });
+    }
+  }, []);
+
+  const searchLocation = () => {
       const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=8d29cd5746ca850b0258d86d3ee349cf&units=metric`;
       axios.get(url).then((response) => {
         setData(response.data);
         console.log(response.data);
       });
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      searchLocation();
     }
   };
 
+
   return (
- <div className="app">
-    <header className="headerTop">
-      <FontAwesomeIcon icon={faBars} className="fa-sharp" />
-      Forecast Mate Weather App</header>
+    <div className="app">
+      <header className="headerTop">
+        <FontAwesomeIcon icon={faBars} className="fa-sharp" />
+        Forecast Mate Weather App</header>
 
-      <p className="headline">
-      <FontAwesomeIcon icon={faSun} className="fa-thin" /> "Plan your day with confidence using our accurate and reliable weather data." 
-       <span className="location-line">(Just Enter Your Location)</span>
-       <FontAwesomeIcon icon={faSun} className="fa-thin" />
-      </p>
+        <p className="headline">
+        <FontAwesomeIcon icon={faSun} className="fa-thin" /> "Plan your day with confidence using our accurate and reliable weather data." 
+         <span className="location-line">(Just Enter Your Location)</span>
+         <FontAwesomeIcon icon={faSun} className="fa-thin" />
+        </p>
 
-   
+     
 
-    <div className="main">
-     <div className="search-input-container">
-      <FontAwesomeIcon className="map-icon" icon={faLocationDot} />
-          <input 
-            className="input"
-            value={location}
-            onChange={event => setLocation(event.target.value)}
-            onKeyUp={searchLocation}
-            placeholder='Enter Your Location'
-            type="text"
-          />
-          <FontAwesomeIcon className="search-icon" icon={faSearch} />
-        </div>
-
-        <div className="image-container">
-        <div className="center">
-          <div className="cloud"></div>
-            <ul className="sun">
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-            </ul>
+      <div className="main">
+       <div className="search-input-container">
+        <FontAwesomeIcon className="map-icon" icon={faLocationDot} />
+            <input 
+              className="input"
+              value={location}
+              onChange={event => setLocation(event.target.value)}
+              onKeyUp={handleKeyPress}
+              placeholder='Enter Your Location'
+              type="text"
+            />
+          
+            <button className="search-button" onClick={searchLocation}>
+            <FontAwesomeIcon className="search-icon" icon={faSearch} />
+              </button>
           </div>
-        </div>
-      
 
-
-     <div className="below-image">
-        <div className="location">
-          <p>{data.name}</p>
-        </div>
-
-          <div className="temp">
-            {data.main ? <h1>{data.main.temp.toFixed()}<span className = "centiSign"> °C</span></h1> : null}
+          <div className="image-container">
+          <div className="center">
+            <div className="cloud"></div>
+              <ul className="sun">
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+              </ul>
+            </div>
           </div>
+        
+
+
+       <div className="below-image">
+          <div className="location">
+            <p>{data.name}</p>
+          </div>
+
+            <div className="temp">
+              {data.main ? <h1>{data.main.temp.toFixed()}<span className = "centiSign"> °C</span></h1> : null}
+            </div>
 
          <div className="description">
             {data.weather ? <p>{data.weather[0].main }</p>: null}
@@ -101,9 +122,7 @@ function App() {
         <p> WindSpeed</p>
       </div>
       </div>
-    </div>
-
-    
+    </div> 
             
   </div>    
    
